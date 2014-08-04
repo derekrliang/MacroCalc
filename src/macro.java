9cc5c82eee051822897e34a90f529a6da6ecd8f6
Derek Liang <frostyrusty@gmail.com>
2014-08-03 22:44:31 -0700

    added protein option functionality, added calculate button, refactored ui for the protein options before final output

--- src/macro.java
+++ src/macro.java
@@ -26,6 +26,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.JButton;
 
 
 public class macro {
@@ -45,7 +46,11 @@ public class macro {
 	private float protein_perc, carb_perc, fat_perc = 20f;
 	private JTextField textField_6;
 	private JTextField textField_7;
+	
+	private int protein_opt = 1;
+	private int fat_slider_old = 20;
 
+	private boolean macro_integrity = true;
 	/**
 	 * Launch the application.
 	 */
@@ -117,6 +122,32 @@ public class macro {
 		textField_2.setText(target_cals + " calories");
 		
 		float protein_f = weightf;
+		
+		if (protein_opt == 2)
+		{
+			float lbm_f = 0;
+			try {
+				lbm_f = Float.parseFloat(textField_6.getText());
+				
+			} catch (NumberFormatException e)
+			{
+				
+			}
+			protein_f = lbm_f;
+		}
+		else if (protein_opt == 3)
+		{
+			float value_f = 0;
+			try {
+				value_f = Float.parseFloat(textField_7.getText());
+				
+			} catch (NumberFormatException e)
+			{
+				
+			}
+			protein_f = value_f;
+		}
+			
 		protein_perc = weightf * 4 * 100 / target_cals;
 	
 		textField_4.setText(String.format("%.2f", protein_f) + "g" + " (" + String.format("%.2f", protein_perc) + "%)");
@@ -130,7 +161,17 @@ public class macro {
 		textField_3.setText(String.format("%.2f", carb_f) + "g" + " (" + String.format("%.2f", carb_perc) + "%)");
 		
 		slider.setValue((int)fat_perc);
+		
+		if (protein_perc >= 0 && fat_perc >= 0 && carb_perc >= 0 && protein_perc + fat_perc + carb_perc >= 99.9 && protein_perc + fat_perc + carb_perc <= 100)
+		{
+			macro_integrity = true;
+		}
+		else
+		{
+			macro_integrity = false;
+		}
 	}
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -138,7 +179,7 @@ public class macro {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 533, 527);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[][][][][][][grow][grow]"));
+		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[][][][][][][grow][grow][][]"));
 		
 		JLabel lblNewLabel = new JLabel("Weight (g)");
 		frame.getContentPane().add(lblNewLabel, "flowy,cell 0 1");
@@ -183,8 +224,74 @@ public class macro {
 		JLabel lblWeightGoal = new JLabel("Weight Goal");
 		frame.getContentPane().add(lblWeightGoal, "flowx,cell 0 4,alignx leading");
 		
+		JPanel panel_11 = new JPanel();
+		frame.getContentPane().add(panel_11, "flowx,cell 0 5");
+		
+		JRadioButton rdbtnLoseWeight = new JRadioButton("Lose");
+		panel_11.add(rdbtnLoseWeight);
+		rdbtnLoseWeight.setSelected(true);
+		loseWeightbtn = rdbtnLoseWeight;
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Gain");
+		panel_11.add(rdbtnNewRadioButton);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnLoseWeight);
+		group.add(rdbtnNewRadioButton);
+		
+		ButtonGroup protein_rb_group = new ButtonGroup();
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, "cell 0 6");
+		panel_1.setLayout(new MigLayout("", "[][grow]", "[][][][]"));
+		
+		JLabel lblNewLabel_4 = new JLabel("Protein");
+		panel_1.add(lblNewLabel_4, "cell 0 0");
+		
+		JRadioButton protein_rb_1 = new JRadioButton("1g per 1lb");
+		protein_rb_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				protein_opt = 1;
+			}
+		});
+		protein_rb_1.setSelected(true);
+		panel_1.add(protein_rb_1, "cell 0 1");
+		
+		JRadioButton protein_rb_2 = new JRadioButton("1g per 1lbm (lbm < lb)");
+		protein_rb_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				protein_opt = 2;
+			}
+		});
+		panel_1.add(protein_rb_2, "cell 0 2");
+		
+		JLabel lblNewLabel_5 = new JLabel("lbm =");
+		panel_1.add(lblNewLabel_5, "flowx,cell 1 2");
+		
+		textField_6 = new JTextField();
+		panel_1.add(textField_6, "cell 1 2,alignx left");
+		textField_6.setColumns(10);
+		
+		JRadioButton protein_rb_3 = new JRadioButton("");
+		protein_rb_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				protein_opt = 3;
+			}
+		});
+		panel_1.add(protein_rb_3, "flowx,cell 0 3");
+		protein_rb_group.add(protein_rb_1);
+		protein_rb_group.add(protein_rb_2);
+		protein_rb_group.add(protein_rb_3);
+		
+		textField_7 = new JTextField();
+		panel_1.add(textField_7, "cell 0 3");
+		textField_7.setColumns(10);
+		
+		JLabel lblG = new JLabel("g");
+		panel_1.add(lblG, "cell 0 3");
+		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, "flowx,cell 0 6,alignx left");
+		frame.getContentPane().add(panel, "flowx,cell 0 8,alignx left");
 		panel.setLayout(new MigLayout("", "[grow][]", "[][][][]"));
 		
 		JLabel lblTargetCalories = new JLabel("Target Calories");
@@ -228,59 +335,32 @@ public class macro {
 			public void stateChanged(ChangeEvent e) {
 				fat_perc = slider.getValue();
 				calculate();
+				
+				if (!macro_integrity)
+				{
+					System.out.println("integrity violation: fatp = " + fat_perc + " proteinp = " + protein_perc + " carbp = " + carb_perc);
+					fat_perc = fat_slider_old;
+					calculate();
+					System.out.println("revert to: fatp = " + fat_perc + " proteinp = " + protein_perc + " carbp = " + carb_perc);
+				}
+				else
+				{
+					fat_slider_old = (int)fat_perc;
+				}
 			}
 		});
 		panel.add(slider, "cell 1 3");
 		
-		JPanel panel_11 = new JPanel();
-		frame.getContentPane().add(panel_11, "flowx,cell 0 5");
+		JPanel panel_2 = new JPanel();
+		frame.getContentPane().add(panel_2, "cell 0 7");
 		
-		JRadioButton rdbtnLoseWeight = new JRadioButton("Lose");
-		panel_11.add(rdbtnLoseWeight);
-		rdbtnLoseWeight.setSelected(true);
-		loseWeightbtn = rdbtnLoseWeight;
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Gain");
-		panel_11.add(rdbtnNewRadioButton);
-		
-		ButtonGroup group = new ButtonGroup();
-		group.add(rdbtnLoseWeight);
-		group.add(rdbtnNewRadioButton);
-		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, "cell 0 7,grow");
-		panel_1.setLayout(new MigLayout("", "[][grow]", "[][][][]"));
-		
-		JLabel lblNewLabel_4 = new JLabel("Protein");
-		panel_1.add(lblNewLabel_4, "cell 0 0");
-		
-		JRadioButton protein_rb_1 = new JRadioButton("1g per 1lb");
-		panel_1.add(protein_rb_1, "cell 0 1");
-		
-		JRadioButton protein_rb_2 = new JRadioButton("1g per 1lbm (lbm < lb)");
-		panel_1.add(protein_rb_2, "cell 0 2");
-		
-		JLabel lblNewLabel_5 = new JLabel("lbm =");
-		panel_1.add(lblNewLabel_5, "flowx,cell 1 2");
-		
-		textField_6 = new JTextField();
-		panel_1.add(textField_6, "cell 1 2,alignx left");
-		textField_6.setColumns(10);
-		
-		JRadioButton protein_rb_3 = new JRadioButton("");
-		panel_1.add(protein_rb_3, "flowx,cell 0 3");
-		
-		ButtonGroup protein_rb_group = new ButtonGroup();
-		protein_rb_group.add(protein_rb_1);
-		protein_rb_group.add(protein_rb_2);
-		protein_rb_group.add(protein_rb_3);
-		
-		textField_7 = new JTextField();
-		panel_1.add(textField_7, "cell 0 3");
-		textField_7.setColumns(10);
-		
-		JLabel lblG = new JLabel("g");
-		panel_1.add(lblG, "cell 0 3");
+		JButton btnCalculate = new JButton("Calculate");
+		panel_2.add(btnCalculate);
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				calculate();
+			}
+		});
 	}
 
 }
